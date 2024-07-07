ae8efddfafe1abf9a21788027fb713b475e22279
tlee08 <timothy.e.lee@sydney.edu.au>
2024-07-08 02:52:22 +1000

    abcd

--- microscopy_proc/funcs/post_funcs.py
+++ microscopy_proc/funcs/post_funcs.py
@@ -2,16 +2,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import tifffile
 
-from microscopy_proc.utils.io_utils import make_npy_header, silentremove
-
-# TODO: make with dask
+from microscopy_proc.utils.io_utils import silentremove
 
 
 def make_maxima_scatter(df):
     fig, ax = plt.subplots(figsize=(5, 10))
     sns.scatterplot(x=df["x"], y=df["y"], marker=".", alpha=0.2, s=10, ax=ax)
     ax.invert_yaxis()
+
+
+#####################################################################
+#             Converting coordinates to spatial
+#####################################################################
+
+
+def coords_to_points_workers(arr: np.ndarray, coords: pd.DataFrame):
+    shape = arr.shape
+    # Formatting coord values as (z, y, x) and rounding to integers
+    coords = (
+        coords[["z", "y", "x"]]
+        .round(0)
+        .astype(np.int16)
+        .query(
+            "z >= 0 and z < @shape[0] and y >= 0 and y < @shape[1] and x >= 0 and x < @shape[2]"
+        )
+        .values
+    )
+
+    if coords.shape[0] > 0:
+        arr[coords[:, 0], coords[:, 1], coords[:, 2]] += 1
+    # Return arr
+    return arr
+
+
+def coords_to_points_start(shape):
+    # Initialising spatial array
+    img = np.memmap(
+        "temp.dat",
+        mode="w+",
+        shape=shape,
+        dtype=np.int16,
+    )
+    return img
+
+
+def coords_to_points_end(img, img_out_fp):
+    # Saving the subsampled array
+    tifffile.imwrite(img_out_fp, img)
+    # Removing temporary memmap
+    silentremove("temp.dat")
+    # Returning img array
+    return tifffile.memmap(img_out_fp)
 
 
 #####################################################################
@@ -32,35 +75,14 @@ def coords_to_points(coords: pd.DataFrame, shape: tuple[int, ...], img_out_fp: s
         The output image array
     """
     # Initialising spatial array
-    img = np.memmap(
-        "temp.dat",
-        mode="w+",
-        shape=shape,
-        dtype=np.int16,
-    )
-
-    # Adding coords to image with np.apply_along_axis
-    def f(coord):
-        # Plotting coord to image. Including only coords within the image's bounds
-        if np.all((coord >= 0) & (coord < shape)):
-            z, y, x = coord
-            img[z, y, x] = 1
-
-    # Formatting coord values as (z, y, x) and rounding to integers
-    coords = coords[["z", "y", "x"]].round(0).astype(np.int16)
-    if coords.shape[0] > 0:
-        np.apply_along_axis(f, 1, coords)
+    img = coords_to_points_start(shape)
+    # Adding coords to image
+    coords_to_points_workers(img, coords)
     # Saving the subsampled array
-    np.save(img_out_fp, img)
-    # Making MHD header so it can be read by ImageJ
-    make_npy_header(img_out_fp)
-    # Removing temporary memmap
-    silentremove("temp.dat")
-    # Returning img array
-    return np.load(img_out_fp, mmap_mode="r")
+    coords_to_points_end(img, img_out_fp)
 
 
-def coords_to_heatmaps(coords, r, shape, img_out_fp):
+def coords_to_heatmaps(coords: pd.DataFrame, r, shape, img_out_fp):
     """
     Converts list of coordinates to spatial array as voxels.
     Overlapping areas accumulate in intensity.
@@ -74,98 +96,28 @@ def coords_to_heatmaps(coords, r, shape, img_out_fp):
     Returns:
         The output image array
     """
-    shape = tuple(shape)
-    shape_padded = (shape[0] + r * 2, shape[1] + r * 2, shape[2] + r * 2)
     # Initialising spatial array
-    img = np.memmap(
-        "temp.dat",
-        mode="w+",
-        shape=shape_padded,
-        dtype=np.int16,
-    )
+    img = coords_to_points_start(shape)
+
     # Constructing sphere array mask
     zz, yy, xx = np.ogrid[1 : r * 2, 1 : r * 2, 1 : r * 2]
     circle_arr = np.sqrt((xx - r) ** 2 + (yy - r) ** 2 + (zz - r) ** 2) < r
+    # Constructing offset indices
+    i = np.arange(-r + 1, r)
+    z_ind, y_ind, x_ind = np.meshgrid(i, i, i, indexing="ij")
+    # Adding coords to image
+    for i, j, k, t in zip(
+        z_ind.ravel(), y_ind.ravel(), x_ind.ravel(), circle_arr.ravel()
+    ):
+        if t:
+            coords_i = coords.copy()
+            coords_i["z"] += i
+            coords_i["y"] += j
+            coords_i["x"] += k
+            coords_to_points_workers(img, coords_i)
 
-    # Adding spherical voxels to the image
-    def f(coord):
-        z, y, x = coord
-        # Getting correct img subspace and masking with circle
-        if np.all((coord >= 0) & (coord < shape)):
-            img[z + 1 : z + 2 * r, y + 1 : y + 2 * r, x + 1 : x + 2 * r][
-                circle_arr
-            ] += 1
-
-    # Formatting coord values as (z, y, x) and rounding to integers
-    coords = coords[["z", "y", "x"]].round(0).astype(np.int16)
-    if coords.shape[0] > 0:
-        np.apply_along_axis(f, 1, coords)
     # Saving the subsampled array
-    np.save(img_out_fp, img[r:-r, r:-r, r:-r])
-    # Making MHD header so it can be read by ImageJ
-    make_npy_header(img_out_fp)
-    # Removing temporary memmap
-    silentremove("temp.dat")
-    # Returning img array
-    return np.load(img_out_fp, mmap_mode="r")
-
-
-def coords_to_cells(coords, shape, img_out_fp):
-    """
-    Converts list of coordinates to spatial array as voxels.
-    Overlapping areas accumulate in intensity.
-
-    Params:
-        coords: A pd.DataFrame of points, with the columns, `x`, `y`, and `z`.
-        r: radius of the voxels.
-        shape: The dimensions of the output array. Assumes that shape is in format `(z, y, x)` (regular for npy and tif file).
-        img_out_fp: The output filename.
-
-    Returns:
-        The output image array
-    """
-    # TODO: NEED TO FIX THIS FUNCTION - IT IS STALLING SOMETIMES
-    shape = tuple(shape)
-    r = coords["size"].max()
-    shape_padded = (shape[0] + r * 2, shape[1] + r * 2, shape[2] + r * 2)
-    # Initialising spatial array
-    img = np.memmap(
-        "temp.dat",
-        mode="w+",
-        shape=shape_padded,
-        dtype=np.int16,
-    )
-
-    # Adding spherical voxels to the image
-    def f(coord):
-        z, y, x, source, size = coord
-        coord = np.array((z, y, x))
-        # Constructing sphere array mask
-        zz, yy, xx = np.ogrid[1 : size * 2, 1 : size * 2, 1 : size * 2]
-        circle_arr = (
-            np.sqrt((xx - size) ** 2 + (yy - size) ** 2 + (zz - size) ** 2) < size
-        )
-        # Getting correct img subspace and masking with circle
-        if np.all((coord >= 0) & (coord < shape)):
-            z = z + r - size
-            y = y + r - size
-            x = x + r - size
-            img[z + 1 : z + 2 * size, y + 1 : y + 2 * size, x + 1 : x + 2 * size][
-                circle_arr
-            ] = source
-
-    # Formatting coord values as (z, y, x) and rounding to integers
-    coords = coords[["z", "y", "x", "source", "size"]].round(0).astype(np.int16)
-    if coords.shape[0] > 0:
-        np.apply_along_axis(f, 1, coords)
-    # Saving the subsampled array
-    np.save(img_out_fp, img[r:-r, r:-r, r:-r])
-    # Making MHD header so it can be read by ImageJ
-    make_npy_header(img_out_fp)
-    # Removing temporary memmap
-    silentremove("temp.dat")
-    # Returning img array
-    return np.load(img_out_fp, mmap_mode="r")
+    coords_to_points_end(img, img_out_fp)
 
 
 def coords_to_regions(coords, shape, img_out_fp):
@@ -180,14 +132,8 @@ def coords_to_regions(coords, shape, img_out_fp):
     Returns:
         The output image array
     """
-    shape = tuple(shape)
     # Initialising spatial array
-    img = np.memmap(
-        "temp.dat",
-        mode="w+",
-        shape=shape,
-        dtype=np.int32,
-    )
+    img = coords_to_points_start(shape)
 
     # Adding coords to image with np.apply_along_axis
     def f(coord):
@@ -200,11 +146,6 @@ def coords_to_regions(coords, shape, img_out_fp):
     coords = coords[["z", "y", "x", "id"]].round(0).astype(np.int16)
     if coords.shape[0] > 0:
         np.apply_along_axis(f, 1, coords)
+
     # Saving the subsampled array
-    np.save(img_out_fp, img)
-    # Making MHD header so it can be read by ImageJ
-    make_npy_header(img_out_fp)
-    # Removing temporary memmap
-    silentremove("temp.dat")
-    # Returning img array
-    return np.load(img_out_fp, mmap_mode="r")
+    coords_to_points_end(img, img_out_fp)
--- microscopy_proc/pipelines/post_pipeline.py
+++ microscopy_proc/pipelines/post_pipeline.py
@@ -9,15 +9,15 @@ from microscopy_proc.funcs.post_funcs import coords_to_heatmaps, coords_to_point
 if __name__ == "__main__":
     # Filenames
     in_img_fp = "/home/linux1/Desktop/A-1-1/cellcount/cropped abcd_larger.tif"
-    out_dir = "/home/linux1/Desktop/A-1-1/cellcount"
-    # # Out filenames
+    out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
+    # Out filenames
     regions_arr_fp = os.path.join(out_dir, "regions.zarr")
     maxima_arr_fp = os.path.join(out_dir, "maxima.zarr")
     regions_df_fp = os.path.join(out_dir, "regions.parquet")
     maxima_df_fp = os.path.join(out_dir, "maxima.parquet")
 
     # Defining process params
-    n_workers = 4
+    n_workers = 5
     depth = 50
     chunks = (500, 1400, 1400)
     device_id = 1
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     coords_to_points(
         region_df,
         tifffile.memmap(in_img_fp).shape,
-        os.path.join(out_dir, "points.npy"),
+        os.path.join(out_dir, "points.tif"),
     )
 
     maxima_df = dd.read_parquet(maxima_df_fp).compute()
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         maxima_df,
         5,
         tifffile.memmap(in_img_fp).shape,
-        os.path.join(out_dir, "heatmaps.npy"),
+        os.path.join(out_dir, "heatmaps.tif"),
     )
 
     # Closing client
