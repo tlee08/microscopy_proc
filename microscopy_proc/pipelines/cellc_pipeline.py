d82e900cb9c951af0105d10501786133ff9f8a44
tlee08 <timothy.e.lee@sydney.edu.au>
2024-07-10 05:10:06 +1000

    abcd

--- microscopy_proc/pipelines/cellc_pipeline.py
+++ microscopy_proc/pipelines/cellc_pipeline.py
@@ -1,12 +1,23 @@
 import os
 
 import dask.array as da
+import numpy as np
 from dask.distributed import Client, LocalCluster
 from dask_cuda import LocalCUDACluster
 
+from microscopy_proc.constants import PROC_CHUNKS, S_DEPTH
 from microscopy_proc.funcs.cellc_funcs import (
+    dog_filter,
+    filter_by_size,
+    gaussian_subtraction_filter,
+    get_local_maxima,
+    label_objects_with_sizes,
+    manual_threshold,
+    mask,
     region_to_coords,
+    tophat_filter,
 )
+from microscopy_proc.funcs.tiff_to_zarr import tiff_to_zarr
 from microscopy_proc.utils.dask_utils import block_to_coords, disk_cache, my_trim
 
 if __name__ == "__main__":
@@ -14,14 +25,11 @@ if __name__ == "__main__":
     in_fp = "/home/linux1/Desktop/A-1-1/abcd.tif"
     out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
 
-    # # Custom dask configs
-    # custom_dask_configs()
-
     #########################
     # TIFF TO ZARR
     #########################
 
-    # tiff_to_zarr(in_fp, os.path.join(out_dir, "raw.zarr"), chunks=PROC_CHUNKS)
+    tiff_to_zarr(in_fp, os.path.join(out_dir, "raw.zarr"), chunks=PROC_CHUNKS)
 
     #########################
     # OVERLAP
@@ -29,20 +37,20 @@ if __name__ == "__main__":
 
     # Making Dask cluster and client
     # NOTE: works best threaded (and with few threads)
-    # cluster = LocalCluster(processes=False, threads_per_worker=8)
-    # client = Client(cluster)
-    # print(client.dashboard_link)
+    cluster = LocalCluster(processes=False, threads_per_worker=8)
+    client = Client(cluster)
+    print(client.dashboard_link)
 
-    # # Read raw arr
-    # arr_raw = da.from_zarr(os.path.join(out_dir, "raw.zarr"))
+    # Read raw arr
+    arr_raw = da.from_zarr(os.path.join(out_dir, "raw.zarr"))
 
-    # # Make overlapping blocks
-    # arr_overlap = da.overlap.overlap(arr_raw, depth=S_DEPTH, boundary="reflect")
-    # arr_overlap = disk_cache(arr_overlap, os.path.join(out_dir, "0_overlap.zarr"))
+    # Make overlapping blocks
+    arr_overlap = da.overlap.overlap(arr_raw, depth=S_DEPTH, boundary="reflect")
+    arr_overlap = disk_cache(arr_overlap, os.path.join(out_dir, "0_overlap.zarr"))
 
-    # # Closing client
-    # client.close()
-    # cluster.close()
+    # Closing client
+    client.close()
+    cluster.close()
 
     #########################
     # HEAVY GPU PROCESSING
@@ -54,45 +62,45 @@ if __name__ == "__main__":
     print(client.dashboard_link)
 
     # # Step 0: Read overlapped image
-    # arr_overlap = da.from_zarr(os.path.join(out_dir, "0_overlap.zarr"))
+    arr_overlap = da.from_zarr(os.path.join(out_dir, "0_overlap.zarr"))
 
     # # Step 1: Top-hat filter (background subtraction)
-    # arr_bgrm = arr_overlap.map_blocks(lambda i: tophat_filter(i, 5.0))
-    # arr_bgrm = disk_cache(arr_bgrm, os.path.join(out_dir, "1_bgrm.zarr"))
+    arr_bgrm = arr_overlap.map_blocks(lambda i: tophat_filter(i, 5.0))
+    arr_bgrm = disk_cache(arr_bgrm, os.path.join(out_dir, "1_bgrm.zarr"))
 
     # # Step 2: Difference of Gaussians (edge detection)
-    # arr_dog = arr_bgrm.map_blocks(lambda i: dog_filter(i, 2.0, 4.0))
-    # arr_dog = disk_cache(arr_dog, os.path.join(out_dir, "2_dog.zarr"))
+    arr_dog = arr_bgrm.map_blocks(lambda i: dog_filter(i, 2.0, 4.0))
+    arr_dog = disk_cache(arr_dog, os.path.join(out_dir, "2_dog.zarr"))
 
-    # # Step 3: Gaussian subtraction with large sigma
-    # # (adaptive thresholding - different from top-hat filter)
-    # arr_adaptv = arr_dog.map_blocks(lambda i: gaussian_subtraction_filter(i, 101))
-    # arr_adaptv = disk_cache(arr_adaptv, os.path.join(out_dir, "3_adaptv.zarr"))
+    # Step 3: Gaussian subtraction with large sigma
+    # (adaptive thresholding - different from top-hat filter)
+    arr_adaptv = arr_dog.map_blocks(lambda i: gaussian_subtraction_filter(i, 101))
+    arr_adaptv = disk_cache(arr_adaptv, os.path.join(out_dir, "3_adaptv.zarr"))
 
     # Step 4: Mean thresholding with standard deviation offset
     # NOTE: visually inspect sd offset
-    # arr_adaptv_mean = (
-    #     arr_adaptv.sum() / (np.prod(arr_adaptv.shape()) - arr_adaptv.equals(0))
-    # ).compute()
-    # print(arr_adaptv_mean)
-    # t_p = 30
-    # arr_threshd = arr_adaptv.map_blocks(lambda i: manual_threshold(i, t_p))
-    # arr_threshd = disk_cache(arr_threshd, os.path.join(out_dir, "4_thresh.zarr"))
+    arr_adaptv_mean = (
+        arr_adaptv.sum() / (np.prod(arr_adaptv.shape()) - arr_adaptv.equals(0))
+    ).compute()
+    print(arr_adaptv_mean)
+    t_p = 30
+    arr_threshd = arr_adaptv.map_blocks(lambda i: manual_threshold(i, t_p))
+    arr_threshd = disk_cache(arr_threshd, os.path.join(out_dir, "4_thresh.zarr"))
 
     # Step 5: Object sizes
-    # arr_sizes = arr_threshd.map_blocks(label_objects_with_sizes)
-    # arr_sizes = disk_cache(arr_sizes, os.path.join(out_dir, "5_sizes.zarr"))
+    arr_sizes = arr_threshd.map_blocks(label_objects_with_sizes)
+    arr_sizes = disk_cache(arr_sizes, os.path.join(out_dir, "5_sizes.zarr"))
 
     # Step 6: Filter out large objects (likely outlines, not cells)
     # TODO: Need to manually set min_size and max_size
-    # arr_filt = arr_sizes.map_blocks(lambda i: filter_by_size(i, smin=None, smax=3000))
-    # arr_filt = arr_filt.map_blocks(lambda i: manual_threshold(i, 1))
-    # arr_filt = disk_cache(arr_filt, os.path.join(out_dir, "6_filt.zarr"))
+    arr_filt = arr_sizes.map_blocks(lambda i: filter_by_size(i, smin=None, smax=3000))
+    arr_filt = arr_filt.map_blocks(lambda i: manual_threshold(i, 1))
+    arr_filt = disk_cache(arr_filt, os.path.join(out_dir, "6_filt.zarr"))
 
     # Step 7: Get maxima of image masked by labels
-    # arr_maxima = arr_overlap.map_blocks(lambda i: get_local_maxima(i, 10))
-    # arr_maxima = da.map_blocks(mask, arr_maxima, arr_filt)
-    # arr_maxima = disk_cache(arr_maxima, os.path.join(out_dir, "7_maxima.zarr"))
+    arr_maxima = arr_overlap.map_blocks(lambda i: get_local_maxima(i, 10))
+    arr_maxima = da.map_blocks(mask, arr_maxima, arr_filt)
+    arr_maxima = disk_cache(arr_maxima, os.path.join(out_dir, "7_maxima.zarr"))
 
     # Step 8: Watershed segmentation
     # arr_watershed = da.map_blocks(watershed_segm, arr_overlap, arr_maxima, arr_filt)
