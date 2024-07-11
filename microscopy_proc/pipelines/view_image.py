49bae244ee2b75cf70fc2e242c224050eb487e3c
tlee08 <timothy.e.lee@sydney.edu.au>
2024-07-11 19:47:36 +1000

    abcd

--- microscopy_proc/pipelines/view_image.py
+++ microscopy_proc/pipelines/view_image.py
@@ -9,7 +9,7 @@ from dask.distributed import Client, LocalCluster
 # %%
 
 
-def add_img_f(viewer, arr, vmax):
+def add_img(viewer, arr, vmax):
     viewer.add_image(
         arr,
         # name=arr.__name__,
@@ -18,128 +18,56 @@ def add_img_f(viewer, arr, vmax):
     )
 
 
-def imgs1_f(out_dir):
+def view_imgs(fp_ls, vmax_ls, slicer):
     # Filenames
     out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
 
     # Making Dask cluster and client
-    cluster = LocalCluster()
+    cluster = LocalCluster(processes=False)
     client = Client(cluster)
     print(client.dashboard_link)
 
-    # slicer = (
-    #     slice(None, None, 3),
-    #     slice(None, None, 12),
-    #     slice(None, None, 12),
-    # )
-    slicer = [
-        slice(None),
-        slice(None),
-        slice(None),
+    arr_ls = [
+        da.from_zarr(i)[*slicer].compute()
+        for i in fp_ls
     ]
-    # Reading images
-    arr_overlap = da.from_zarr(os.path.join(out_dir, "0_overlap.zarr"))[
-        *slicer
-    ].compute()
-    arr_bgrm = da.from_zarr(os.path.join(out_dir, "1_bgrm.zarr"))[*slicer].compute()
-    arr_dog = da.from_zarr(os.path.join(out_dir, "2_dog.zarr"))[*slicer].compute()
-    arr_adaptv = da.from_zarr(os.path.join(out_dir, "3_adaptv.zarr"))[*slicer].compute()
-    # arr_threshd = da.from_zarr(os.path.join(out_dir, "4_threshd.zarr"))[*slicer].compute()
-    arr_sizes = da.from_zarr(os.path.join(out_dir, "5_sizes.zarr"))[*slicer].compute()
-    arr_filt = da.from_zarr(os.path.join(out_dir, "6_filt.zarr"))[*slicer].compute()
-    arr_maxima = da.from_zarr(os.path.join(out_dir, "7_maxima.zarr"))[*slicer].compute()
 
     client.close()
     cluster.close()
 
     # Napari viewer adding images
     viewer = napari.Viewer()
-    # add_img_f(viewer, arr_raw, 10000)
-    add_img_f(viewer, arr_overlap, 10000)
-    add_img_f(viewer, arr_bgrm, 2000)
-    add_img_f(viewer, arr_dog, 100)
-    add_img_f(viewer, arr_adaptv, 100)
-    # add_img_f(viewer, arr_threshd, 5)
-    add_img_f(viewer, arr_sizes, 10000)
-    add_img_f(viewer, arr_filt, 5)
-    add_img_f(viewer, arr_maxima, 5)
+    for i, j in zip(arr_ls, vmax_ls):
+        add_img(viewer, i, j)
 
     napari.run()
 
-
-def imgs2_f(out_dir):
-    # Filenames
-    out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
-
-    # Making Dask cluster and client
-    cluster = LocalCluster()
-    client = Client(cluster)
-    print(client.dashboard_link)
-
-    # slicer = (
-    #     slice(None, None, 3),
-    #     slice(None, None, 12),
-    #     slice(None, None, 12),
-    # )
-    slicer = [
-        slice(None),
-        slice(None),
-        slice(None),
-    ]
-    # Reading images
-    arr_raw = da.from_zarr(os.path.join(out_dir, "raw.zarr"))[*slicer].compute()
-    arr_filt_f = da.from_zarr(os.path.join(out_dir, "9_filt_f.zarr"))[*slicer].compute()
-    arr_maxima_f = da.from_zarr(os.path.join(out_dir, "9_maxima_f.zarr"))[
-        *slicer
-    ].compute()
-
-    client.close()
-    cluster.close()
-
-    # Napari viewer adding images
-    viewer = napari.Viewer()
-    add_img_f(viewer, arr_raw, 10000)
-    add_img_f(viewer, arr_filt_f, 5)
-    add_img_f(viewer, arr_maxima_f, 5)
-
-    napari.run()
-    
-def imgs3_f(out_dir):
-    # Filenames
-    out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
-
-    # Making Dask cluster and client
-    cluster = LocalCluster()
-    client = Client(cluster)
-    print(client.dashboard_link)
-
-    slicer = (
-        slice(None, None, 3),
-        slice(None, None, 12),
-        slice(None, None, 12),
-    )
-    # Reading images
-    arr_raw = da.from_zarr(os.path.join(out_dir, "raw.zarr"))[*slicer].compute()
-    # arr_filt_f = da.from_zarr(os.path.join(out_dir, "9_filt_f.zarr"))[*slicer].compute()
-    # arr_maxima_f = da.from_zarr(os.path.join(out_dir, "9_maxima_f.zarr"))[
-    #     *slicer
-    # ].compute()
-
-    client.close()
-    cluster.close()
-
-    # Napari viewer adding images
-    viewer = napari.Viewer()
-    add_img_f(viewer, arr_raw, 10000)
-    # add_img_f(viewer, arr_filt_f, 5)
-    # add_img_f(viewer, arr_maxima_f, 5)
-
-    napari.run()
 
 
 if __name__ == "__main__":
     # Filenames
     out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
 
-    # imgs1_f(out_dir)
-    imgs3_f(out_dir)
+    slicer = (
+        slice(None, None, 3),
+        slice(None, None, 12),
+        slice(None, None, 12),
+    )
+
+    imgs_ls = (
+        ("raw", 10000),
+        ("overlap", 10000),
+        # ("bgrm", 2000),
+        # ("dog", 100),
+        # ("adaptv", 100),
+        # ("threshd", 5),
+        # ("sizes", 10000),
+        # ("filt", 5),
+        # ("maxima", 5),
+        # ("filt_f", 5),
+        # ("maxima_f", 5), 
+    )
+    fp_ls = [os.path.join(out_dir, f"{i}.zarr") for i, j in imgs_ls]
+    vmax_ls = [j for i, j in imgs_ls]
+
+    view_imgs(fp_ls, vmax_ls, slicer)
