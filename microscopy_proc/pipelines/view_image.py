00d451ae34037f737dcf35bcaf961f3e1ed9c332
tlee08 <timothy.e.lee@sydney.edu.au>
2024-07-11 16:54:41 +1000

    abcd

--- microscopy_proc/pipelines/view_image.py
+++ microscopy_proc/pipelines/view_image.py
@@ -8,7 +8,66 @@ from dask.distributed import Client, LocalCluster
 
 # %%
 
-if __name__ == "__main__":
+
+def add_img_f(viewer, arr, vmax):
+    viewer.add_image(
+        arr,
+        # name=arr.__name__,
+        contrast_limits=(0, vmax),
+        blending="additive",
+    )
+
+
+def imgs1_f(out_dir):
+    # Filenames
+    out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
+
+    # Making Dask cluster and client
+    cluster = LocalCluster()
+    client = Client(cluster)
+    print(client.dashboard_link)
+
+    # slicer = (
+    #     slice(None, None, 3),
+    #     slice(None, None, 12),
+    #     slice(None, None, 12),
+    # )
+    slicer = [
+        slice(None),
+        slice(None),
+        slice(None),
+    ]
+    # Reading images
+    arr_overlap = da.from_zarr(os.path.join(out_dir, "0_overlap.zarr"))[
+        *slicer
+    ].compute()
+    arr_bgrm = da.from_zarr(os.path.join(out_dir, "1_bgrm.zarr"))[*slicer].compute()
+    arr_dog = da.from_zarr(os.path.join(out_dir, "2_dog.zarr"))[*slicer].compute()
+    arr_adaptv = da.from_zarr(os.path.join(out_dir, "3_adaptv.zarr"))[*slicer].compute()
+    # arr_threshd = da.from_zarr(os.path.join(out_dir, "4_threshd.zarr"))[*slicer].compute()
+    arr_sizes = da.from_zarr(os.path.join(out_dir, "5_sizes.zarr"))[*slicer].compute()
+    arr_filt = da.from_zarr(os.path.join(out_dir, "6_filt.zarr"))[*slicer].compute()
+    arr_maxima = da.from_zarr(os.path.join(out_dir, "7_maxima.zarr"))[*slicer].compute()
+
+    client.close()
+    cluster.close()
+
+    # Napari viewer adding images
+    viewer = napari.Viewer()
+    # add_img_f(viewer, arr_raw, 10000)
+    add_img_f(viewer, arr_overlap, 10000)
+    add_img_f(viewer, arr_bgrm, 2000)
+    add_img_f(viewer, arr_dog, 100)
+    add_img_f(viewer, arr_adaptv, 100)
+    # add_img_f(viewer, arr_threshd, 5)
+    add_img_f(viewer, arr_sizes, 10000)
+    add_img_f(viewer, arr_filt, 5)
+    add_img_f(viewer, arr_maxima, 5)
+
+    napari.run()
+
+
+def imgs2_f(out_dir):
     # Filenames
     out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
 
@@ -29,34 +88,26 @@ if __name__ == "__main__":
     ]
     # Reading images
     arr_raw = da.from_zarr(os.path.join(out_dir, "raw.zarr"))[*slicer].compute()
-    arr_overlap = da.from_zarr(os.path.join(out_dir, "0_overlap.zarr"))[
+    arr_filt_f = da.from_zarr(os.path.join(out_dir, "9_filt_f.zarr"))[*slicer].compute()
+    arr_maxima_f = da.from_zarr(os.path.join(out_dir, "9_maxima_f.zarr"))[
         *slicer
     ].compute()
-    arr_bgrm = da.from_zarr(os.path.join(out_dir, "1_bgrm.zarr"))[*slicer].compute()
-    arr_dog = da.from_zarr(os.path.join(out_dir, "2_dog.zarr"))[*slicer].compute()
-    arr_adaptv = da.from_zarr(os.path.join(out_dir, "3_adaptv.zarr"))[*slicer].compute()
-    # arr_threshd = da.from_zarr(os.path.join(out_dir, "4_threshd.zarr"))[*slicer].compute()
-    arr_sizes = da.from_zarr(os.path.join(out_dir, "5_sizes.zarr"))[*slicer].compute()
-    arr_filt = da.from_zarr(os.path.join(out_dir, "6_filt.zarr"))[*slicer].compute()
 
     client.close()
     cluster.close()
 
-    add_img_f = lambda arr, vmax: viewer.add_image(
-        arr,
-        # name=arr.__name__,
-        contrast_limits=(0, vmax),
-        blending="additive",
-    )
     # Napari viewer adding images
     viewer = napari.Viewer()
-    # img_raw = add_img_f(arr_raw, 10000)
-    img_overlap = add_img_f(arr_overlap, 10000)
-    img_bgrm = add_img_f(arr_bgrm, 500)
-    img_dog = add_img_f(arr_dog, 100)
-    img_adaptv = add_img_f(arr_adaptv, 100)
-    # img_threshd = add_img_f(arr_threshd, 1)
-    img_sizes = add_img_f(arr_sizes, 10000)
-    img_filt = add_img_f(arr_filt, 1)
+    add_img_f(viewer, arr_raw, 10000)
+    add_img_f(viewer, arr_filt_f, 5)
+    add_img_f(viewer, arr_maxima_f, 5)
 
     napari.run()
+
+
+if __name__ == "__main__":
+    # Filenames
+    out_dir = "/home/linux1/Desktop/A-1-1/large_cellcount"
+
+    # imgs1_f(out_dir)
+    imgs2_f(out_dir)
