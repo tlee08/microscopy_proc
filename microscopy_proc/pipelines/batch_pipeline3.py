95a2d825d8f87dd94ae1f120ba70fd62226ff6ac
tlee08 <timothy.e.lee@sydney.edu.au>
2024-09-29 02:02:02 +1000

    abcd

--- microscopy_proc/pipelines/batch_pipeline3.py
+++ microscopy_proc/pipelines/batch_pipeline3.py
@@ -9,11 +9,6 @@ from microscopy_proc.pipelines.map_pipeline import (
     grouping_cells,
     transform_coords,
 )
-from microscopy_proc.pipelines.reg_pipeline import (
-    img_trim_pipeline,
-    ref_prepare_pipeline,
-    registration_pipeline,
-)
 from microscopy_proc.utils.proj_org_utils import (
     get_proj_fp_dict,
     get_ref_fp_dict,
@@ -32,26 +27,15 @@ if __name__ == "__main__":
     assert in_fp_dir != batch_proj_dir
 
     for i in natsorted(os.listdir(in_fp_dir)):
-        # Only given files
-        if i not in [
-            "G5_agg_2.5x_1xzoom_05072024",
-            "G13_2.5x_1x_zoom_07082024",
-            # "P6_2.5x_1x_zoom_08082024",
-            # "P8_2.5x_1x_zoom_07082024",
-            # "P12_2.5x_1x_zoom_07082024",
-            # "P13_2.5x_1x_zoom_05082024",
-            # "P14_2.5x_1x_zoom_08082024",
-            # "P15_2.5x_1x_zoom_07082024",
-            # "P16_2.5x_1x_zoom_06082024",
-            # "R5_agg_2.5x_1xzoom_03072024",
-            # "R6_agg_2.5x_1xzoom_05072024",
-            # "R13_agg_2.5x_1xzoom_04072024",
-            # "R14_agg_2.5x_1xzoom_02072024",
-            # "R15_agg_2.5x_1xzoom_04072024",
-            # "R16_reimage_agg_2.5x_1xzoom_05072024",
-            # "R18_agg_2.5x_1xzoom_03072024",
-        ]:
-            continue
+        # # Only given files
+        # if i not in [
+        #     "B3_2.5x_1x_zoom_08082024",
+        #     "B9_2.5x_1x_zoom_06082024",
+        #     "G5_agg_2.5x_1xzoom_05072024",
+        #     "G8_2.5x_1x_zoom_08082024",
+        #     "G13_2.5x_1x_zoom_07082024",
+        # ]:
+        #     continue
         # Checking if it is a directory
         if not os.path.isdir(os.path.join(in_fp_dir, i)):
             continue
@@ -79,35 +63,35 @@ if __name__ == "__main__":
 
             # if not os.path.exists(proj_fp_dict["regresult"]):
             # Preparing reference images
-            ref_prepare_pipeline(
-                ref_fp_dict=ref_fp_dict,
-                proj_fp_dict=proj_fp_dict,
-                # ref_orient_ls=(-2, 3, 1),
-                # ref_z_trim=(None, None, None),
-                # ref_y_trim=(None, None, None),
-                # ref_x_trim=(None, None, None),
-            )
+            # ref_prepare_pipeline(
+            #     ref_fp_dict=ref_fp_dict,
+            #     proj_fp_dict=proj_fp_dict,
+            #     # ref_orient_ls=(-2, 3, 1),
+            #     # ref_z_trim=(None, None, None),
+            #     # ref_y_trim=(None, None, None),
+            #     # ref_x_trim=(None, None, None),
+            # )
             # Preparing image itself
-            # prepare_img_rough(
+            # img_rough_pipeline(
             #     proj_fp_dict,
             #     # z_rough=3,
             #     # y_rough=6,
             #     # x_rough=6,
             # )
-            # prepare_img_fine(
+            # img_fine_pipeline(
             #     proj_fp_dict,
             #     # z_fine=1,
             #     # y_fine=0.6,
             #     # x_fine=0.6,
             # )
-            img_trim_pipeline(
-                proj_fp_dict,
-                # z_trim=(None, None, None),
-                # y_trim=(None, None, None),
-                # x_trim=(None, None, None),
-            )
+            # img_trim_pipeline(
+            #     proj_fp_dict,
+            #     # z_trim=(None, None, None),
+            #     # y_trim=(None, None, None),
+            #     # x_trim=(None, None, None),
+            # )
             # Running Elastix registration
-            registration_pipeline(proj_fp_dict)
+            # registration_pipeline(proj_fp_dict)
 
             # if not os.path.exists(proj_fp_dict["cells_raw_df"]):
             #     # Making overlapped chunks images for processing
