0697a0dc0ff8c733e71d194e98dd0fcd82d3bf67
tlee08 <87008424+tlee08@users.noreply.github.com>
2024-09-29 22:13:15 +0200

    Update map_pipeline.py

--- microscopy_proc/pipelines/map_pipeline.py
+++ microscopy_proc/pipelines/map_pipeline.py
@@ -33,14 +33,14 @@ def transform_coords(pfm: dict):
     """
     with cluster_proc_contxt(LocalCluster(n_workers=4, threads_per_worker=1)):
         # Getting registration parameters
-        rp = ConfigParamsModel.model_validate(read_json(pfm["config_params"]))
+        rp = ConfigParamsModel.model_validate(read_json(pfm.config_params))
         # Setting output key (in the form "<maxima/region>_trfm_df")
         # Getting cell coords
-        cells_df = dd.read_parquet(pfm["cells_raw_df"]).compute()
+        cells_df = dd.read_parquet(pfm.cells_raw_df).compute()
         # Sanitising (removing smb columns)
         cells_df = sanitise_smb_df(cells_df)
         # Taking only "z", "y", "x" coord columns
-        cells_df = cells_df[["z", "y", "x"]]
+        cells_df = cells_df[.z", "y", "x]
         # Scaling to resampled rough space
         # NOTE: this downsampling uses slicing so must be computed differently
         cells_df = cells_df / np.array((rp.z_rough, rp.y_rough, rp.x_rough))
@@ -51,7 +51,7 @@ def transform_coords(pfm: dict):
             [s[0] if s[0] else 0 for s in (rp.z_trim, rp.y_trim, rp.x_trim)]
         )
 
-        cells_df = transformation_coords(cells_df, pfm["ref"], pfm["regresult"])
+        cells_df = transformation_coords(cells_df, pfm.ref, pfm.regresult)
         # NOTE: Using pandas parquet. does not work with dask yet
         # cells_df = dd.from_pandas(cells_df, npartitions=1)
         # Fitting resampled space to atlas image with Transformix (from Elastix registration step)
@@ -59,9 +59,9 @@ def transform_coords(pfm: dict):
         #     npartitions=int(np.ceil(cells_df.shape[0].compute() / ROWSPPART))
         # )
         # cells_df = cells_df.map_partitions(
-        #     transformation_coords, pfm["ref"], pfm["regresult"]
+        #     transformation_coords, pfm.ref, pfm.regresult
         # )
-        cells_df.to_parquet(pfm["cells_trfm_df"])
+        cells_df.to_parquet(pfm.cells_trfm_df)
 
 
 # @flow
@@ -74,25 +74,25 @@ def get_cell_mappings(pfm: dict):
     """
     with cluster_proc_contxt(LocalCluster()):
         # Reading cells_raw and cells_trfm dataframes
-        cells_df = dd.read_parquet(pfm["cells_raw_df"]).compute()
-        coords_trfm = pd.read_parquet(pfm["cells_trfm_df"])
+        cells_df = dd.read_parquet(pfm.cells_raw_df).compute()
+        coords_trfm = pd.read_parquet(pfm.cells_trfm_df)
         # Sanitising (removing smb columns)
         cells_df = sanitise_smb_df(cells_df)
         coords_trfm = sanitise_smb_df(coords_trfm)
         # Making unique incrementing index
         cells_df = cells_df.reset_index(drop=True)
         # Setting the transformed coords
-        cells_df["z_trfm"] = coords_trfm["z"].values
-        cells_df["y_trfm"] = coords_trfm["y"].values
-        cells_df["x_trfm"] = coords_trfm["x"].values
+        cells_df.z_trfm = coords_trfm.z.values
+        cells_df.y_trfm = coords_trfm.y.values
+        cells_df.x_trfm = coords_trfm.x.values
 
         # Reading annotation image
-        arr_annot = tifffile.imread(pfm["annot"])
+        arr_annot = tifffile.imread(pfm.annot)
         # Getting the annotation ID for every cell (zyx coord)
         # Getting transformed coords (that are within tbe arr bounds, and their corresponding idx)
         s = arr_annot.shape
         trfm_loc = (
-            cells_df[["z_trfm", "y_trfm", "x_trfm"]]
+            cells_df[.z_trfm", "y_trfm", "x_trfm]
             .round(0)
             .astype(np.int32)
             .query(
@@ -101,20 +101,20 @@ def get_cell_mappings(pfm: dict):
         )
         # Getting the pixel values of each valid transformed coord (hence the specified index).
         # Invalids are set to -1
-        cells_df["id"] = pd.Series(
+        cells_df.id = pd.Series(
             arr_annot[*trfm_loc.values.T].astype(np.uint32),
             index=trfm_loc.index,
         ).fillna(-1)
 
         # Reading annotation mappings dataframe
-        with open(pfm["map"], "r") as f:
-            annot_df = nested_tree_dict2df(json.load(f)["msg"][0])
+        with open(pfm.map, "r") as f:
+            annot_df = nested_tree_dict2df(json.load(f).msg[0])
         # Getting the annotation name for every cell (zyx coord)
         cells_df = df_map_ids(cells_df, annot_df)
         # Saving to disk
         # NOTE: Using pandas parquet. does not work with dask yet
         # cells_df = dd.from_pandas(cells_df)
-        cells_df.to_parquet(pfm["cells_df"])
+        cells_df.to_parquet(pfm.cells_df)
 
 
 def grouping_cells(pfm: dict):
@@ -126,7 +126,7 @@ def grouping_cells(pfm: dict):
     """
     with cluster_proc_contxt(LocalCluster()):
         # Reading cells dataframe
-        cells_df = pd.read_parquet(pfm["cells_df"])
+        cells_df = pd.read_parquet(pfm.cells_df)
         # Sanitising (removing smb columns)
         cells_df = sanitise_smb_df(cells_df)
         # Grouping cells by region name
@@ -137,8 +137,8 @@ def grouping_cells(pfm: dict):
         )
         # Reading annotation mappings dataframe
         # Making df of region names and their parent region names
-        with open(pfm["map"], "r") as f:
-            annot_df = nested_tree_dict2df(json.load(f)["msg"][0])
+        with open(pfm.map, "r") as f:
+            annot_df = nested_tree_dict2df(json.load(f).msg[0])
         # Combining (summing) the cells_grouped_df values for parent regions using the annot_df
         cells_grouped_df = combine_nested_regions(cells_grouped_df, annot_df)
         # Calculating integrated average intensity (sum_intensity / size)
@@ -149,16 +149,16 @@ def grouping_cells(pfm: dict):
         # Saving to disk
         # NOTE: Using pandas parquet. does not work with dask yet
         # cells_grouped = dd.from_pandas(cells_grouped)
-        cells_grouped_df.to_parquet(pfm["cells_agg_df"])
+        cells_grouped_df.to_parquet(pfm.cells_agg_df)
 
 
 def cells2csv(pfm: dict):
     # Reading cells dataframe
-    cells_df = pd.read_parquet(pfm["cells_df"])
+    cells_df = pd.read_parquet(pfm.cells_df)
     # Sanitising (removing smb columns)
     cells_df = sanitise_smb_df(cells_df)
     # Saving to csv
-    cells_df.to_csv(pfm["cells_csv"])
+    cells_df.to_csv(pfm.cells_csv)
 
 
 if __name__ == "__main__":
